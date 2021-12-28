13c16ae3903a486f5cc8a2afe4b79bf41cc79957
Andrii-debug <75580226+Andrii-debug@users.noreply.github.com>
2021-12-28 22:39:50 +0200

    delete from array, and save content fron array

--- index.js
+++ index.js
@@ -1,77 +1,129 @@
-const button = document.querySelector('.btn-1');
-const input = document.querySelector('.todo_input');
-const task = document.querySelector('.task')
-const btnSave = document.querySelector('.btn__save')
-const ClearBtn = document.querySelector('.btn__clear')
+const button = document.querySelector(".btn-add");
+const input = document.querySelector(".todo_input");
+const task = document.querySelector(".task");
+const btnSave = document.querySelector(".btn-save");
+const ClearBtn = document.querySelector(".btn-clear");
+
+let tasks = [];
+
 
 function loadTodos() {
-    const data = localStorage.getItem('task')
-    if (data) {
-        task.innerHTML = data;
+  const data = JSON.parse(localStorage.getItem("task"));
+
+  if (data) {
+    for (let i = 0; i < data.length; i++) {
+      let render = data[i];
+      function returnEL() {
+        let li = document.createElement("li");
+        let b1 = document.createElement("button");
+
+        li.className = "case";
+        li.textContent = render;
+           
+        b1.className = "btn";
+        b1.innerHTML = "";
+
+        task.appendChild(li);
+        li.appendChild(b1);
+
+        tasks.push(render); // тут вставили очередной элемент
+        let task_index = tasks.length - 1; // узнаем его индекс по длине массива, так как он в текущей ситуации последний
+        // несколько строк спустя
+        b1.addEventListener("click",  (function(inner_task_index)  { // функция-обертка для изоляции контекста, сюда передается значение task_index, я специально выделил его другим именем inner_task_index, чтобы видеть контекст функции-обертки
+    
+        // обертка возвращает функцию-обработчик события  для addEventListener
+        return function delFromLocalSt() {
+            task.removeChild(li);
+                let array = JSON.parse(localStorage.getItem('task'));
+                array.splice(inner_task_index, 1); // удаляем элемент по известному индексу используя механизм замыкания, используя контекст функции обертки
+            localStorage.setItem('task', JSON.stringify(array));
+            location.reload()
+           }
+          
+        })
+        (task_index) // вызываем хитрую обертку, чтобы обеспечить изоляцию переменной от контекста функции createEl
+        
+        );
+
+       
+
+        li.addEventListener("click", () => {
+          li.classList.toggle("case-active");
+        });
+      }
+      returnEL();
     }
- 
+  }
 }
+
 //////////////////// listenners ///////////////
 
-button.addEventListener('click', function addTask() {
-    if (input.value === '') {
-        alert('Write a task')
-    } else {     
-        createEl()
-        input.value = ''
-    }
-})
-
-btnSave.addEventListener('click', function Save() {
-    localStorage.setItem('task', task.innerHTML)
-})
-
-
-ClearBtn.addEventListener('click', function Clear() {
-    task.innerHTML = ''
-    localStorage.removeItem('task', task)
-})
-
-input.addEventListener('keypress', function pressEnter(e) {
-    if (e.key === 'Enter') {
-        if (input.value === '') {
-            alert('Write a task')
-        } else {     
-            createEl()
-            input.value = ''
-        }
-    }
+button.addEventListener("click", function addTask() {
+  if (input.value === "") {
+    alert("Write a task");
+  } else {
+    createEl();
+    input.value = "";
+    localStorage.setItem("task", JSON.stringify(tasks));
+  }
 });
 
-window.addEventListener('load', loadTodos)
+ClearBtn.addEventListener("click", function Clear() {
+  task.innerHTML = "";
+  tasks = []
+  localStorage.removeItem("task", task);
+});
+
+input.addEventListener("keypress", function pressEnter(e) {
+  if (e.key === "Enter") {
+    if (input.value === "") {
+      alert("Write a task");
+    } else {
+      createEl();
+      input.value = "";
+      localStorage.setItem("task", JSON.stringify(tasks));
+     
+    }
+  }
+});
 
 ///////////////////////////////////////////////
 
-function createEl() {
+function createEl(data) {
+  let li = document.createElement("li");
+  let b1 = document.createElement("button");
+  data = input.value;
+
+  li.className = "case";
+  li.textContent = data;
+  b1.className = "btn";
+  b1.innerHTML = "";
+
+  task.appendChild(li);
+  li.appendChild(b1);
+
+    tasks.push(data); // тут вставили очередной элемент
+    let task_index = tasks.length - 1; // узнаем его индекс по длине массива, так как он в текущей ситуации последний
+    // несколько строк спустя
+    b1.addEventListener("click",  (function(inner_task_index)  { // функция-обертка для изоляции контекста, сюда передается значение task_index, я специально выделил его другим именем inner_task_index, чтобы видеть контекст функции-обертки
+
+    // обертка возвращает функцию-обработчик события  для addEventListener
+    return function delFromLocalSt() {
+        task.removeChild(li);
+            let array = JSON.parse(localStorage.getItem('task'));
+            array.splice(inner_task_index, 1); // удаляем элемент по известному индексу используя механизм замыкания, используя контекст функции обертки
+        localStorage.setItem('task', JSON.stringify(array));
+        // location.reload()
+       }
+    })
+    (task_index) // вызываем хитрую обертку, чтобы обеспечить изоляцию переменной от контекста функции createEl
     
-    let li = document.createElement('li')
-    let b1 = document.createElement('button') 
+    );
 
-
-     li.className = 'case';
-     li.textContent = input.value;
-     
-     b1.className = 'btn';
-     b1.innerHTML = '';
-
-    task.appendChild(li);
-    li.appendChild(b1);
-     
-
-     b1.addEventListener('click', () => {
-        task.removeChild(li)
-         
-     })
-
-     li.addEventListener('click', () => {
-        li.classList.toggle('case-active')
-       
-     })
+  li.addEventListener("click", () => {
+    li.classList.toggle("case-active");
+  })
 
 }
 
+loadTodos()
